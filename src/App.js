cd580180625f130e6198c554f55fa6223f7b51d3
HShambuck <shambuckhalifax@gmail.com>
2024-11-01 04:28:57 +0000

    Created ShopContext and Used it for ShopCategory.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,9 @@ import { Product } from './Pages/Product';
 import { LoginSignup } from './Pages/LoginSignup';
 import { Cart } from './Pages/Cart';
 import { Footer } from './Components/Footer/Footer';
-
-console.log("Navbar:", Navbar);
-console.log("Shop:", Shop);
-console.log("ShopCategory:", ShopCategory);
-console.log("Product:", Product);
-console.log("LoginSignup:", LoginSignup);
-console.log("Cart:", Cart);
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -22,9 +18,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
         <Route path='/product' element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
